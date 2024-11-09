ab597c9a1904bd85630ccc01229182d19f16f034
t.puzanov <tamirpuzanov@gmail.com>
2024-11-09 13:13:13 +0700

    save

--- task2/main.py
+++ task2/main.py
@@ -2,36 +2,36 @@ import fn, math
 
 
 def F(x):
-    return (x ** 3) * math.sin(x)
+    return math.sin(x) + ((x ** 3) * math.sin(x)) ** 2
 
 
 def main():
 
-    a, b = 0, math.pi / 2
+    a, b = -math.pi, math.pi
 
     N = 20
 
     X1 = fn.split(N * 1, a, b)
     X2 = fn.split(N * 2, a, b)
 
-    t = (math.pi ** 2 - 8) * 3 / 4
+    t = math.pi ** 7 / 7 - 3 * math.pi * (15 + 2 * (math.pi ** 2) * (math.pi ** 2 - 5)) / 4
 
     fns = {
         "Left Rectangles": fn.left_rectangle,
         "Central Rectangles": fn.central_rectangle,
         "Trapezoid": fn.trapezoid,
         "Simpson": fn.simpson,
-        "Newton (k=1)": fn.newton2,
-        "Newton (k=2)": fn.newton3
+        "Gauss (k=1)": fn.newton2,
+        "Gauss (k=2)": fn.newton3
     }
 
     p = {
-        "Left Rectangles": 1,
-        "Central Rectangles": 2,
-        "Trapezoid": 2,
+        "Left Rectangles": 4,
+        "Central Rectangles": 4,
+        "Trapezoid": 4,
         "Simpson": 4,
-        "Newton (k=1)": 4,
-        "Newton (k=2)": 6
+        "Gauss (k=1)": 4,
+        "Gauss (k=2)": 6
     }
 
     tLen = max(map(len, fns.keys()))
@@ -39,15 +39,15 @@ def main():
     table = fn.create_table(fns)
     cols  = table[0]
 
-    d1 = fn.calc(cols, fns, F, N * 1, X1)
-    d2 = fn.calc(cols, fns, F, N * 2, X2)
+    c1 = fn.calc(cols, fns, F, N * 1, X1)
+    c2 = fn.calc(cols, fns, F, N * 2, X2)
 
     table.append(fn.calc(cols, fns, F, N * 1, X1, t))
     table.append(fn.calc(cols, fns, F, N * 2, X2, t))
 
     table.append(fn.power(table[1], table[2]))
 
-    table.append(fn.improve(table, d1, d2, p, t))
+    table.append(fn.improve(table, c1, c2, p, t))
 
     fn.print_table(table, tLen, [15, 15, 2, 15])
 
